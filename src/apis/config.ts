cf1f02d0462f7c38b2e5954746a2a2a63b616c9a
nohseongbong <shtjdqhd@naver.com>
2023-08-04 20:49:44 +0900

    feat: axios 관련 세팅 추가

---
+++ src/apis/config.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { apiUrl } from "@/share/libs/common";
+
+export const axiosInstance = axios.create({
+  baseURL: `${apiUrl}`,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    config.headers = config.headers ?? {};
+    return config;
+  },
+  (error) => {
+    console.log("Request Api Error");
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  async (response) => {
+    return response;
+  },
+  async (error) => {
+    console.log("Response Api Error");
+    return Promise.reject(error);
+  }
+);
