18f3099b7825600f5b8f752deb346b5fab0a2201
nohseongbong <shtjdqhd@naver.com>
2023-08-13 13:32:42 +0900

    feat: users Api 데이터 바인딩 작업

--- src/components/main/MainContainer.tsx
+++ src/components/main/MainContainer.tsx
@@ -1,30 +1,31 @@
-import { runInAction } from "mobx";
-import { observer } from "mobx-react-lite";
 import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
 
-import TestModel from "@/model/testModel";
-import { listItemStore } from "@/store/mobx/listItemStore";
-import { appStore } from "@/store/mobx/appStore";
+import { getUsers } from "@/apis/user/user";
+import { useUserStore } from "@/store/zustand/userStore";
+import { useEffect, useState } from "react";
 
-const MainContainer = observer(() => {
-  const onClickItem = (item: TestModel) => {
-    runInAction(() => {
-      listItemStore.model = item;
-    });
-  };
+const MainContainer = () => {
+  const { data } = useQuery({ queryKey: ["users"], queryFn: getUsers, refetchOnWindowFocus: false });
+  const { actions, users } = useUserStore((store) => store);
+
+  useEffect(() => {
+    if (data) actions.setUsersData(data.users);
+  }, [data]);
   return (
     <div>
-      {appStore.list.map((item, index) => (
-        <div key={index}>
-          <Link onClick={() => onClickItem(item)} href={`/main/${item.text}`}>
-            {item.text}
-            디테일 이동
-          </Link>
+      {users.map((item) => (
+        <div key={item.userId} style={{ display: "flex", flexDirection: "column" }}>
+          <img style={{ width: "100px", height: "100px" }} src={item.avatar} />
+          <span>이름 : {item.username}</span>
+          <span>이메일 : {item.email}</span>
+          <span>생일 : {item.birthdate}</span>
+          <Link href={`/main/${item.username}`}>디테일 이동</Link>
         </div>
       ))}
       <span>Main page</span>
     </div>
   );
-});
+};
 
 export default MainContainer;
